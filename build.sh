7449f0d0286f108d2793c97184b4b6e47b8128ea
Vaclav Petras <wenzeslaus@gmail.com>
2016-02-15 16:02:24 -0500

    build slides in different screen ratios

--- build.sh
+++ build.sh
@@ -3,9 +3,30 @@
 build=build
 project_582=PetrasVaclav_ProjectPresentation
 
-pdflatex $project_582
-pdflatex $project_582
-pdflatex $project_582
+# how many times repeat latex
+n=3
 
-cp $project_582.pdf $build/$project_582.pdf
-cp index.html $build/index.html
+function tex_to_pdf {
+    for i in $(eval echo {1..$n}); do pdflatex $1; done
+}
+
+set -e
+
+sed -e "s/aspectratio=1610/aspectratio=169/g" < ${project_582}.tex > ${project_582}_16x9.tex
+sed -e "s/aspectratio=1610/aspectratio=43/g" < ${project_582}.tex > ${project_582}_4x3.tex
+
+tex_to_pdf ${project_582} &
+tex_to_pdf ${project_582}_16x9 &
+tex_to_pdf ${project_582}_4x3 &
+wait
+
+echo done
+
+cp ${project_582}.pdf $build
+cp ${project_582}_16x9.pdf $build
+cp ${project_582}_4x3.pdf $build
+cp ${project_582}.pdf $build/${project_582}_16x10.pdf
+
+cp index.html ${build}/index.html
+
+rm ${project_582}_16x9.* ${project_582}_4x3.*
